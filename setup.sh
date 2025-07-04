baff98c5a2c7dffe44c9b0d430670165166285a2
Almond Jetson <jetson@almondbot.com>
2025-07-04 21:33:25 +0000

    ensure apriltag location is visible to dynamic linker

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ cmake -B build \
     -DBUILD_PYTHON_WRAPPER=ON \
     -DPython3_EXECUTABLE=$(which python)
 sudo cmake --build build --target install
+
+echo "/usr/local/lib" | sudo tee /etc/ld.so.conf.d/usr-local.conf
+sudo ldconfig
+ldconfig -p | grep libapriltag
